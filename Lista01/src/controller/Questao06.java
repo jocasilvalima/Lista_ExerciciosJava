bc3b73b546b1557204f2716eb1c10f346db9cb07
João Carllos <jocasilvalima@gmail.com>
2023-07-02 13:25:13 -0300

    Correção das variaveis

--- Lista01/src/controller/Questao06.java
+++ Lista01/src/controller/Questao06.java
@@ -6,11 +6,11 @@ public class Questao06 {
 
 	public static void main(String[] args) {
 		
-		double gfahrenheit = Teclado.lerDouble("Escreva a mediada em graus Fahrenheit para ser convertida em graus celsius.");
+		double gFahrenheit = Teclado.lerDouble("Escreva a mediada em graus Fahrenheit para ser convertida em graus celsius.");
 		
-		double gcelsius = (gfahrenheit - 32) * 5/9;
+		double gCelsius = (gFahrenheit - 32) * 5/9;
 		
-		System.out.println("O grau " + gfahrenheit + "Fº convertido para celsius é: " + gcelsius + "Cº");
+		System.out.println("O grau " + gFahrenheit + "Fº convertido para celsius é: " + gCelsius + "Cº");
 	}
 
 }
