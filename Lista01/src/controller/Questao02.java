a71ef232445fc169c33bdb43b4ff595fdfef668b
João Carllos <jocasilvalima@gmail.com>
2023-07-02 11:41:00 -0300

    Acrescentei mais dois numeros

--- Lista01/src/controller/Questao02.java
+++ Lista01/src/controller/Questao02.java
@@ -8,12 +8,14 @@ public class Questao02 {
 
 		int primeiron = Teclado.lerInt("Digite o primeiro numero: ");
 		int segundon = Teclado.lerInt("Digite o segundo numero: ");
+		int terceiron = Teclado.lerInt("Digite o terceiro numero: ");
+		int quarton = Teclado.lerInt("Digite o quarto numero: ");
 
-		int multiplicacao = primeiron * segundon;
-		int divisao = primeiron / segundon;
-		int adicao = primeiron + segundon;
-		int subtracao = primeiron - segundon;
-		int restodadivisao = primeiron % segundon;
+		int multiplicacao = primeiron * segundon * terceiron * quarton;
+		int divisao = primeiron / segundon / terceiron / quarton;
+		int adicao = primeiron + segundon + terceiron + quarton;
+		int subtracao = primeiron - segundon - terceiron - quarton;
+		int restodadivisao = primeiron % segundon % terceiron % quarton;
 
 		System.out.println("A multiplicação é: " + multiplicacao);
 		System.out.println("A divisão é: " + divisao);
